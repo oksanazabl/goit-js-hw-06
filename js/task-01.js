579f8cbb05ef0fa905460108e15314d5de858a85
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-01-25 11:46:57 +0200

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,11 @@
 
-const ulElem = document.querySelectorAll('#categories>li');
-ulElem.forEach(Element => {
-  console.log(
-    `Category: ${Element.firstElementChild.textContent},
-    Elements: ${Element.lastElementChild.children.length}`,
-  );
-});
 
-const liElem = document.querySelectorAll("li.item");
-console.log(`Number of categories:${liElem.length}`);
+const elemLi = document.querySelectorAll(".item");
+console.log(`Number of categories: ${elemLi.length}`);
+
+const elemUl = document.querySelectorAll("#categories>li");
+elemUl.forEach(Element => {
+  console.log(`Category: ${Element.firstElementChild.textContent}`);
+  console.log(`Elements: ${Element.lastElementChild.children.length}`);
+});
 
