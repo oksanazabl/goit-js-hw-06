2d4ba8969c8194f47e0e941f1272ac1df039a6e8
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-01-27 22:52:22 +0200

    task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,18 @@
 let valueStart = 0;
 const counterValue = document.querySelector("#value");
-const btnAdd = document.querySelector("#counter").firstElementChild;
-const btnMinus = document.querySelector("#counter").lastElementChild;
+const btnMore = document.querySelector("#counter").firstElementChild;
+const btnLess = document.querySelector("#counter").lastElementChild;
 
+const clickAdd = () => {
+    valueStart += 1;
+
+    counterValue.innerHTML = valueStart
+};
+btnLess.addEventListener('click', clickAdd);
+
+const clickLess = () => {
+    valueStart -= 1;
+
+    counterValue.innerHTML = valueStart
+};
+btnMore.addEventListener('click', clickLess);
