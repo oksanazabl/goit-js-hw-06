46932e88c21a47e7ef5c5d29ebb870ead9ec790f
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-01-27 18:15:57 +0200

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,5 @@
+let valueStart = 0;
+const counterValue = document.querySelector("#value");
+const btnAdd = document.querySelector("#counter").firstElementChild;
+const btnMinus = document.querySelector("#counter").lastElementChild;
+
