dbb65d0d0fcc70f5a2297dd93f11f9d8c3f80ecb
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-01-28 10:09:07 +0200

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const body = document.querySelector("body")
+const btn = document.querySelector('button')
+const colorValue = document.querySelector('span.color')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onClick = () => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color
+}
+
+btn.addEventListener('click', onClick)
+
+
